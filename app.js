0393b5781904de7dc564de944b22fba9a39da28a
J0C4P <j0c4pa1bar@gmail.com>
2026-03-14 19:40:45 -0300

    Corrección error de conexión a mondo
    Removed source comments from app.js

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// Source - https://stackoverflow.com/a/79874273
-// Posted by Vin
-// Retrieved 2026-03-14, License - CC BY-SA 4.0
-
 require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 
 
